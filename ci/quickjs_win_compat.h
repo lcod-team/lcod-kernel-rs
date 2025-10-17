0af3af3da252ae5cf37bfabef3697da5c4f1b0cb
Nicolas Albert <nicolas.albert@gmail.com>
2025-10-17 13:42:55 +0200

    ci: shim sys/time for windows quickjs (#18)

--- ci/quickjs_win_compat.h
+++ ci/quickjs_win_compat.h
@@ -4,26 +4,10 @@
 #ifdef _WIN32
 #  include <winsock2.h>
 #  include <windows.h>
-#  include <sys/timeb.h>
 #  include <malloc.h>
 
 #  ifndef alloca
 #    define alloca _alloca
-#  endif
-
-static inline int qjs_gettimeofday(struct timeval *tv) {
-    struct _timeb tb;
-    if (_ftime_s(&tb) != 0) {
-        return -1;
-    }
-    tv->tv_sec = (long)tb.time;
-    tv->tv_usec = (long)tb.millitm * 1000;
-    return 0;
-}
-
-#  ifndef HAVE_GETTIMEOFDAY
-#    define HAVE_GETTIMEOFDAY 1
-#    define gettimeofday(tv, tz) (void)(tz), qjs_gettimeofday((tv))
 #  endif
 
 #endif /* _WIN32 */
